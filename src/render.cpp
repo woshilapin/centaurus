f501c7f9279cd0fe1cb7e6d9a0ac0faff3d359ae
woshilapin <woshilapin@gmail.com>
2013-01-06 20:27:40 +0100

    Create the Render class
    The Render class will be the renderer of the ray tracer. This is just an
    initialization of the class at this time.

---
+++ src/render.cpp
@@ -0,0 +1,15 @@
+#include "render.hpp"
+
+using namespace centaurus;
+
+Render::Render()
+{
+}
+
+Render::Render(const Render & src)
+{
+}
+
+Render::~Render()
+{
+}
---
+++ src/render.hpp
@@ -0,0 +1,15 @@
+#ifndef __RENDER_HPP__
+#define __RENDER_HPP__
+
+namespace centaurus
+{
+	class Render
+	{
+		public:
+			Render();
+			Render(const Render &);
+			~Render();
+	};
+}
+
+#endif // __RENDER_HPP__
