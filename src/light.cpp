763724a13d149c9880d57c1302cd560733276bb1
woshilapin <woshilapin@gmail.com>
2013-01-06 21:35:41 +0100

    Add the Light class
    This is only a skeleton of the Light class.

---
+++ src/light.cpp
@@ -0,0 +1,15 @@
+#include "light.hpp"
+
+using namespace centaurus;
+
+Light::Light()
+{
+}
+
+Light::Light(const Light & src)
+{
+}
+
+Light::~Light()
+{
+}
---
+++ src/light.hpp
@@ -0,0 +1,16 @@
+#ifndef __LIGHT_HPP__
+#define __LIGHT_HPP__
+
+namespace centaurus
+{
+	class Light
+	{
+		private:
+		public:
+			Light();
+			Light(const Light &);
+			~Light();
+	};
+}
+
+#endif // __LIGHT_HPP__
