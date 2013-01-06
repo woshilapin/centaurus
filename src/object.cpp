71620139cf2b4c668605e4a9c186598633bf1392
woshilapin <woshilapin@gmail.com>
2013-01-06 21:36:22 +0100

    Add the Object class
    This is only a skeleton of the Object class.

---
+++ src/object.cpp
@@ -0,0 +1,15 @@
+#include "object.hpp"
+
+using namespace centaurus;
+
+Object::Object()
+{
+}
+
+Object::Object(const Object & src)
+{
+}
+
+Object::~Object()
+{
+}
---
+++ src/object.hpp
@@ -0,0 +1,16 @@
+#ifndef __OBJECT_HPP__
+#define __OBJECT_HPP__
+
+namespace centaurus
+{
+	class Object
+	{
+		private:
+		public:
+			Object();
+			Object(const Object &);
+			~Object();
+	};
+}
+
+#endif // __OBJECT_HPP__
