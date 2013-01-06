550d2bf4eb8c2f3c2c543139ca589e60c8c5badf
woshilapin <woshilapin@gmail.com>
2013-01-06 21:34:41 +0100

    Add the Camera class
    This is only a skeleton of the Camera class.

---
+++ src/camera.cpp
@@ -0,0 +1,15 @@
+#include "camera.hpp"
+
+using namespace centaurus;
+
+Camera::Camera()
+{
+}
+
+Camera::Camera(const Camera & src)
+{
+}
+
+Camera::~Camera()
+{
+}
---
+++ src/camera.hpp
@@ -0,0 +1,16 @@
+#ifndef __CAMERA_HPP__
+#define __CAMERA_HPP__
+
+namespace centaurus
+{
+	class Camera
+	{
+		private:
+		public:
+			Camera();
+			Camera(const Camera &);
+			~Camera();
+	};
+}
+
+#endif // __CAMERA_HPP__
