a8cdd07382fa60cc9ba03c1ba1bdc35f7e1174fd
Jean SIMARD <woshilapin@tuziwo.info>
2016-01-17 00:58:02 +0100

    Add GPLv3 license to every source files

--- src/centaurus.cpp
+++ src/centaurus.cpp
@@ -1,20 +1,25 @@
-/**
+/*
  * @file
  * @author  woshilapin <woshilapin@tuziwo.info>
  * @version 0.1
  *
  * @section LICENSE
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of
- * the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details at
- * http://www.gnu.org/copyleft/gpl.html
+ * centaurus - a relativist ray-tracer
+ * Copyright © 2012-2016 woshilapin <woshilapin@tuziwo.info>
+ * 
+ * centaurus is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * centaurus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with centaurus.  If not, see <http://www.gnu.org/licenses/>.
  *
  * @section This is a small description of the centaurus relativist ray-tracer.
  *
@@ -33,9 +38,11 @@ using namespace centaurus;
 int main (const int argc, const char* const argv[])
 {
 	init_localization (PACKAGE, LOCALEDIR);
-	cout << "Executing " << argv[0];
-	cout << " with " << argc-1 << " argument(s)" << endl;
-	cout << "This is " << PACKAGE_STRING << endl;
+	cout << PACKAGE_NAME << " [" << PACKAGE_VERSION << "]" << endl;
+	cout << "Copyright \u00A9 2012-2016 woshilapin <woshilapin@tuziwo.info>" << endl;
+	cout << "This program comes with ABSOLUTELY NO WARRANTY." << endl;
+	cout << "This is free software, and you are welcome to redistribute it" << endl;
+	cout << "under certain conditions." << endl;
 
 	render r;
 	r.run();
--- src/object.hpp
+++ src/object.hpp
@@ -1,3 +1,20 @@
+/*
+ * centaurus - a relativist ray-tracer
+ * Copyright © 2012-2016 woshilapin <woshilapin@tuziwo.info>
+ * 
+ * centaurus is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * centaurus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with centaurus.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #ifndef __OBJECT_HPP__
 #define __OBJECT_HPP__
 
