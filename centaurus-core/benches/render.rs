37e50866339567bf711db51791de462875e767a1
Jean Simard <jean.simard@dominos.com>
2018-07-11 21:00:52 -0400

    [bench] Set up a benchmark

---
+++ centaurus-core/benches/render.rs
@@ -0,0 +1,21 @@
+extern crate centaurus_core;
+#[macro_use]
+extern crate criterion;
+
+use centaurus_core::SceneBuilder;
+use criterion::Criterion;
+
+fn render_over_image_size(width: usize, height: usize) {
+    let scene = SceneBuilder::new().with_width(width).with_height(height).build();
+    scene.render();
+}
+
+fn render_benchmark(c: &mut Criterion) {
+    c.bench_function_over_inputs(
+        "render over image size",
+        |b, &&size| b.iter(|| render_over_image_size(size, size)),
+        &[8, 16, 32, 64, 128]);
+}
+
+criterion_group!(benches, render_benchmark);
+criterion_main!(benches);
