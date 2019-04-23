2093ec2cb28f4822432c00a4ad7a5ebe61b9912b
Jean SIMARD <woshilapin@tuziwo.info>
2019-04-23 16:02:14 -0400

    Add rustdoc to Color

--- centaurus_core/src/image/color.rs
+++ centaurus_core/src/image/color.rs
@@ -1,13 +1,18 @@
 use serde_derive::Deserialize;
 
+/// Define a RGB color
 #[derive(Debug, Default, Deserialize, Copy, Clone)]
 pub struct Color {
+    /// Red intensity as an integer from 0 to 256
     pub red: u8,
+    /// Green intensity as an integer from 0 to 256
     pub green: u8,
+    /// Blue intensity as an integer from 0 to 256
     pub blue: u8,
 }
 
 impl Color {
+    /// Instantiate a new Color
     pub fn new(red: u8, green: u8, blue: u8) -> Color {
         Color { red, green, blue }
     }
