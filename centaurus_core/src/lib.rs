8d64852034e24cc08875cf09c6d1f515e7f6b166
Jean SIMARD <woshilapin@tuziwo.info>
2019-04-23 23:35:06 -0400

    Support multiple lights

--- centaurus_core/src/lib.rs
+++ centaurus_core/src/lib.rs
@@ -27,6 +27,22 @@ pub struct Scene {
     pub objects: Vec<Triangle>,
 }
 
+fn bounded_add(v1: u8, v2: u8) -> u8 {
+    if (v1 as u16 + v2 as u16) < (u8::max_value() as u16) {
+        v1 + v2
+    } else {
+        u8::max_value()
+    }
+}
+fn combine_color(c1: Rgba<u8>, c2: Rgba<u8>) -> Rgba<u8> {
+    Rgba([
+        bounded_add(c1[0], c2[0]),
+        bounded_add(c1[1], c2[1]),
+        bounded_add(c1[2], c2[2]),
+        bounded_add(c1[3], c2[3]),
+    ])
+}
+
 impl Scene {
     pub fn render(&self) -> RgbaImage {
         let _camera = Camera::new(
@@ -35,33 +51,39 @@ impl Scene {
             1.0,
             [1.0, -1.0, -1.0, 1.0],
         );
-        let light = Sun::new(Vector3::new(0.5, 0.5, 1.0));
-        let __light = Lightbulb::new(Point3::new(1.0, 1.0, -2.0));
-        let _light = Spot::new(
-            Point3::new(0.5, 0.5, -1.0),
-            Vector3::new(0.0, 0.0, 1.0),
+        let light1 = Sun::new(Vector3::new(0.5, 0.5, 1.0));
+        let light2 = Spot::new(
+            Point3::new(0.5, 0.5, -2.0),
+            Vector3::new(-0.5, -0.5, 1.0),
             0.2,
         );
+        let light3 = Lightbulb::new(Point3::new(-1.0, 1.0, 0.0));
+        let lights: Vec<Box<Light>> = vec![Box::new(light1), Box::new(light2), Box::new(light3)];
         let progress_bar = ProgressBar::new(self.height as u64 * self.width as u64);
         RgbaImage::from_fn(self.width, self.height, |i, j| {
             progress_bar.inc(1);
             let x = 2.0 * (i as f64) / ((self.width - 1) as f64) - 1.0;
             let y = 2.0 * ((self.height - j) as f64) / ((self.height - 1) as f64) - 1.0;
             let ray = Ray::new(Point3::new(x, y, -1.0), Vector3::new(0.0, 0.0, 1.0));
+            let mut color = Rgba(self.background_color);
             for object in &self.objects {
                 if let Some(intersection) = object.intersect(&ray) {
                     let i_position = intersection.position;
                     let i_normal = intersection.normal;
-                    if let Some(l_direction) = light.light_direction(&i_position) {
-                        let intensity = i_normal.dot(&(-l_direction));
-                        if intensity >= 0.0 && intensity <= 1.0 {
-                            let intensity = (intensity * f64::from(u8::max_value())) as u8;
-                            return Rgba([intensity, intensity, intensity, u8::max_value()]);
+                    for light in &lights {
+                        if let Some(l_direction) = light.light_direction(&i_position) {
+                            let intensity = i_normal.dot(&(-l_direction));
+                            if intensity >= 0.0 && intensity <= 1.0 {
+                                let intensity = (intensity * f64::from(u8::max_value())) as u8;
+                                let new_color =
+                                    Rgba([intensity, intensity, intensity, u8::max_value()]);
+                                color = combine_color(color, new_color);
+                            }
                         }
                     }
                 }
             }
-            Rgba(self.background_color)
+            color
         })
     }
 }
